8c23526ce4a499f947a675ca50540832069a52cc
deantook <chenyunlong99@outlook.com>
2025-06-15 00:22:36 +0800

    grpc

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -9,7 +9,8 @@ export default navbar([
         children: [
             "golang/",
             "sre/",
-            "flutter/"
+            "flutter/",
+            "grpc/"
         ],
     },
 ]);
